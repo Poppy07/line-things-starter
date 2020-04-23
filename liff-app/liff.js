d15defd0ed899986b550895465e7d8d9d5f84ff1
Poppy <saowalakru56@email.nu.ac.th>
2020-04-24 00:08:26 +0700

    Update liff.js

--- liff-app/liff.js
+++ liff-app/liff.js
@@ -1,5 +1,5 @@
 // User service UUID: Change this to your generated service UUID
-const USER_SERVICE_UUID         = 'd2ae5e7a-53a4-473f-b0c5-7526e431718c'; // LED, Button
+const USER_SERVICE_UUID         = 'f20d9b98-797d-495c-a63f-002e53bb95ed'; // LED, Button
 // User service characteristics
 const LED_CHARACTERISTIC_UUID   = 'E9062E71-9E62-4BC6-B0D3-35CDCD9B027B';
 const BTN_CHARACTERISTIC_UUID   = '62FBD229-6EDD-4D1A-B554-5C4E1BB29169';
